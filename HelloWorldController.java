618991f6dbbdeb8ace3870dd0775d2d6e396c303
snehaj12 <89454333+snehaj12@users.noreply.github.com>
2021-08-26 14:57:02 +0530

    Add files via upload
    updated controller msg

---
+++ HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.mavericks.helloWorld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping("/home")
+	public String getHomePage() {
+		return "Hello World, from Team Mavericks";
+	}
+}
