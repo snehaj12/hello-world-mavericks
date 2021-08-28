4521953d64de05ec17f6d9790fa12a4b07464faa
snehaj12 <89454333+snehaj12@users.noreply.github.com>
2021-08-29 02:27:00 +0530

    Add files via upload

--- src/main/java/com/mavericks/helloWorld/controller/HelloWorldController.java
+++ src/main/java/com/mavericks/helloWorld/controller/HelloWorldController.java
@@ -1,16 +1,13 @@
 package com.mavericks.helloWorld.controller;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
 	@GetMapping("/home")
-	public String getHomePage(@RequestParam(name="name", required=false, defaultValue="Mavericks") String name, Model model) {
-		model.addAttribute("name", name);
-		return "welcome.html";
+	public String getHomePage() {
+		return "Hello World, from Team Mavericks. This is version6";
 	}
 }
